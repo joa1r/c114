b1829595414ee7766e50ed7f2eae1ea45c7eaccd
joao <110570660+joa1r@users.noreply.github.com>
2023-09-05 15:28:32 -0300

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,14 @@
+a = 33
+b = "33"
+
+if b > a:
+    print("b maior que a")
+elif b == a:
+    print("b e a são iguais")
+
+def my_function(fname):
+    print(fname + " Refsnes")
+
+    my_function("Emilly")
+    my_function("Tobias")
+    my_function("Lucas")
